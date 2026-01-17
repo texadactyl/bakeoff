e2d4d0daf2bc41627600dcffd76fb42a84e6ef09
Richard Elkins <richard.elkins@gmail.com>
2026-01-17 16:28:53 -0600

    Add files via upload

--- run.sh
+++ run.sh
@@ -4,29 +4,35 @@ unset JAVA_TOOL_OPTIONS
 
 exec > >(tee "$LOGFILE") 2>&1
 echo =============== C ============================
+gcc --version | head -n 1
 gcc -O3 -march=native -mtune=native -ffast-math main.c
 a.out
 echo
 echo =============== D \(ldc2\) =====================
+ldc2 --version | head -n 1
 ldc2 -O3 -release -mcpu=native -ffast-math main.d -of=d.out
 d.out
 echo
 echo =============== zig ==========================
+echo zig `zig version`
 zig build-exe main.zig -O ReleaseFast -mcpu=native
 mv main ziggy.out
 ziggy.out
 echo
 echo =============== Go ===========================
+go version
 go build -o go.out main.go
 go.out
 echo
 echo =============== Java Compile ================================================
+javac --version
 javac -Xlint -Werror main.java
 echo =============== Hotspot -server =============================================
+java --version | head -n 1
 java -server main
 echo
 echo =============== Hotspot -server -XX:+TieredCompilation ======================
+java --version | head -n 1
 java -server -XX:+TieredCompilation main
 echo
-echo =============== Jacobin ======================
-java main
+
